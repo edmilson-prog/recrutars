8186c8585c5b8163fad1a5ea923e4b9796253cfa
edmilson-prog <edmilson@ailainteligente.com>
2026-04-30 15:24:40 -0300

    chore: bump version to 1.58.0 Mirror

--- src/constants/app.ts
+++ src/constants/app.ts
@@ -4,8 +4,8 @@
  * PRD-044: Pagina "Sobre" e Tooltip de Versao
  */
 
-export const APP_VERSION = "1.57.0";
-export const APP_CODENAME = "Tailor";
+export const APP_VERSION = "1.58.0";
+export const APP_CODENAME = "Mirror";
 export const APP_NAME = "RecrutaRS";
 export const APP_COMPANY = "AILA - Sistemas Inteligentes";
 export const APP_COMPANY_URL = "https://ailainteligente.com";
