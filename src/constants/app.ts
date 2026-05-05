1f085b6b261b7cd2efee2b0daa70fa06fa6b8e7d
edmilson-prog <edmilson@ailainteligente.com>
2026-05-04 21:23:19 -0300

    chore: bump version to 1.59.0 'Insight' (AI Match)

--- src/constants/app.ts
+++ src/constants/app.ts
@@ -4,8 +4,8 @@
  * PRD-044: Pagina "Sobre" e Tooltip de Versao
  */
 
-export const APP_VERSION = "1.58.4";
-export const APP_CODENAME = "Mirror";
+export const APP_VERSION = "1.59.0";
+export const APP_CODENAME = "Insight";
 export const APP_NAME = "RecrutaRS";
 export const APP_COMPANY = "AILA - Sistemas Inteligentes";
 export const APP_COMPANY_URL = "https://ailainteligente.com";
