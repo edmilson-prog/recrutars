928ff62a7705af84f3c3995ef991ad142c8646ff
edmilson-prog <edmilson@ailainteligente.com>
2026-05-06 17:03:41 -0300

    chore(release): bump to v1.61.0 Dossier (1.60.0 already taken by Curator)

--- src/constants/app.ts
+++ src/constants/app.ts
@@ -4,7 +4,7 @@
  * PRD-044: Pagina "Sobre" e Tooltip de Versao
  */
 
-export const APP_VERSION = "1.60.0";
+export const APP_VERSION = "1.61.0";
 export const APP_CODENAME = "Dossier";
 export const APP_NAME = "RecrutaRS";
 export const APP_COMPANY = "AILA - Sistemas Inteligentes";
